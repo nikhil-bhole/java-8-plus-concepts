217543dcfe37bae87b0f85901a647411c7f8259b
Nikhil Bhole <nrbhole21@gmail.com>
2023-03-06 11:57:03 +0530

    Update ListNegatives.java

--- src/streams_concepts/ListNegatives.java
+++ src/streams_concepts/ListNegatives.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ListNegatives {
+    
     public static void main(String[] args) {
 
         List<Integer> numbers = Arrays.asList(5, 12, -9, 9, 3, -15, 88);
@@ -14,4 +15,5 @@ public class ListNegatives {
         List<Integer> _negatives = numbers.stream().filter(e -> Integer.toString(e).contains("-")).toList();
         System.out.println(_negatives);
     }
+    
 }
