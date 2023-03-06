fe21610f28b1efe757f820ee9c65fb0d1ca42473
Nikhil Bhole <nrbhole21@gmail.com>
2023-03-06 17:45:34 +0530

    Create SumAll.java

---
+++ src/streams_concepts/SumAll.java
@@ -0,0 +1,15 @@
+package streams_concepts;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumAll {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(5, 12, -9, 9, 9, 3, -15, 88,5);
+
+        int sum = numbers.stream().reduce(0,Integer::sum);
+
+        System.out.println(sum);
+    }
+}
